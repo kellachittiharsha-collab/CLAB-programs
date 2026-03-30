71f871d75d2336978b4f39dce94267dded4761a5
kellachittiharsha-collab <kellachittiharsha@gmail.com>
2026-03-30 22:35:06 +0530

    Update and rename if.c to input and output.c

---
+++ input and output.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter the age of harsha:");
+    scanf("%d",&age);
+    printf("The age of the harsha is %d years old",age);
+    
+    return 0;
+}
+
+
